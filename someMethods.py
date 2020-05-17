75b9e79f71ed7767947413ca07231229cf66e447
duygu bulut <DuyguBulut@users.noreply.github.com>
2020-05-17 16:48:51 +0300

    Update and rename sort.py to someMethods.py

---
+++ someMethods.py
@@ -0,0 +1,25 @@
+
+#method of sort
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full, reverse=True)
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place)
+print(place_up)
+
+# Print out the number of o's in place
+z = place.count('o')
+print(z)
